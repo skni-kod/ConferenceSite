d691260881052e9e65ab8a502706540236c8abb4
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-02-19 00:57:51 +0100

    style: remove needless spaces

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -47,9 +47,8 @@ const Navigation = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
             >
-              {' '}
-              <path stroke="none" d="M0 0h24v24H0z" />{' '}
-              <line x1="18" y1="6" x2="6" y2="18" />{' '}
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <line x1="18" y1="6" x2="6" y2="18" />
               <line x1="6" y1="6" x2="18" y2="18" />
             </svg>
           )}
