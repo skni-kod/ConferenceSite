2a8232d7128a1bd2c6a28bd168bfb299431d9b51
Maksymilian Dendura <maksd0410@gmail.com>
2022-02-19 20:10:32 +0100

    Updated faq content

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -14,6 +14,18 @@ const Faq = () => {
         question="Jak mogę uzyskać bilet na konferencję stacjonarną?"
         answer="Napisz do Nas na mail załączony w zakładce Kontakt. Jeśli zdążysz przed innymi to otrzymasz darmową wejściówkę na wydarzenie!"
       />
+      <FaqItem
+        question="Nie mogę przyjechać na konferencję mimo że posiadam bilet. Co wtedy?"
+        answer="Zawsze możesz uczestniczyć w konferencji zdalnie! Transmisje wydarzenia znajdziesz na naszych mediach społecznościowych oraz na stronie wydarzenia."
+      />
+      <FaqItem
+        question="Nie działa mi transmisja online. Co zrobić?"
+        answer="Spróbuj otworzyć transmisje na innym medium. Jeśli to nie pomoże, napisz do Nas a postaramy się Ci pomóc!"
+      />
+      <FaqItem
+        question="Co to za konferencja?"
+        answer="Nasze wydarzenie jest konferencją branżową. Podczas licznych prelekcji specjaliści z wieloletnim doświadczeniem w IT opowiedzą o wielu aspektach pracy w branży, a studenci z kół naukowych pokażą ciekawe projekty wykonywane w czasie studiów. "
+      />
     </section>
   )
 }
