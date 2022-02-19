019f4cfcfdc03a467a4485e2abd33148e5c745e0
Maksymilian Dendura <maksd0410@gmail.com>
2022-02-19 21:45:15 +0100

    Improvements of ScheduleItem mobile view

--- components/ScheduleItem.tsx
+++ components/ScheduleItem.tsx
@@ -6,9 +6,9 @@ export type ScheduleItemProps = {
 
 const ScheduleItem = ({ event, hour }: ScheduleItemProps) => {
   return (
-    <div className="my-3 flex justify-between rounded-xl bg-blue-50 odd:bg-blue-100  py-2 px-4 text-sm  max-w-5xl mx-auto sm:text-lg md:my-4 lg:px-12 lg:py-3 lg:text-2xl">
-      <div>{hour}</div>
-      <div>{event}</div>
+    <div className="my-3 flex justify-between rounded-xl bg-blue-50 odd:bg-blue-100  py-2 px-4 text-sm gap-4 max-w-5xl mx-auto sm:text-lg md:my-4 lg:px-12 lg:py-3 lg:text-2xl">
+      <div className="whitespace-nowrap">{hour}</div>
+      <div className="text-right">{event}</div>
     </div>
   )
 }
