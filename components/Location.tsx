08de0e57453e18b6cb08cbbd6d5196b387981959
Maksymilian Dendura <maksd0410@gmail.com>
2022-02-19 20:23:18 +0100

    Added location aria-label

--- components/Location.tsx
+++ components/Location.tsx
@@ -14,6 +14,7 @@ const Location = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2563.590293749265!2d21.9870063157158!3d50.019033979418204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473cfbb343ad0907%3A0xf25c32d349275186!2sBudynek%20V!5e0!3m2!1spl!2spl!4v1645294596091!5m2!1spl!2spl"
         loading="lazy"
         className='w-full h-96'
+        aria-label="lokalizacja"
       ></iframe>
     </section>
   )
