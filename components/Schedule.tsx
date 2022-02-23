77153aa319a71f51e97b41fe496f91656e59ea57
Maksymilian Dendura <maksd0410@gmail.com>
2022-02-23 23:17:03 +0100

    Updated schedule content

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -10,34 +10,31 @@ const Schedule = () => {
         Harmonogram wydarzenia
       </h2>
       <div>
-        <ScheduleItem hour="10:00 - 10:20" event="Rozpoczęcie konferencji" />
-        <ScheduleItem hour="10:25 - 11:00" event="Jak zacząć w branży IT" />
+        <ScheduleItem hour="10:00 - 10:30" event="Rozpoczęcie konferencji" />
         <ScheduleItem
-          hour="11:05 - 11:35"
-          event="Prezentacja Systemu operacyjnego MicrOS"
+          hour="10:30 - 11:00"
+          event="Prezentacja projektu Roguelike"
+          organiser='SKNI "KOD"'
         />
         <ScheduleItem
-          hour="11:40 - 12:25"
-          event="Prezentacja przedstawiciela firmy Deloite"
+          hour="11:05 - 11:50"
+          event='Prelekcja "Jak wystartować w IT?"'
+          organiser="Fabrity"
         />
         <ScheduleItem
-          hour="12:45 - 13:15"
-          event="Prezentacja dotycząca cyberbezpieczeństwa"
+          hour="11:55 - 12:25"
+          event="Prezentacja projektu MicrOS"
+          organiser='SKNI "KOD"'
         />
+        <ScheduleItem hour="12:30 - 13:00" event="Prelekcja - WKRÓTCE" />
+        <ScheduleItem hour="13:05 - 13:50" event="Prelekcja - WKRÓTCE" />
+        <ScheduleItem hour="14:15 - 14:45" event="Prelekcja - WKRÓTCE" />
+        <ScheduleItem hour="14:50 - 15:30" event="Prelekcja - WKRÓTCE" />
+        <ScheduleItem hour="15:35 - 16:15" event="Prelekcja - WKRÓTCE" />
         <ScheduleItem
-          hour="13:20 - 14:05"
-          event="Prezentacja przedstawiciela firmy Ideo"
+          hour="16:15 - 16:30"
+          event="Zakończenie konferencji i rozdanie nagród uczestnikom"
         />
-        <ScheduleItem hour="14:10 - 14:40" event="Prezentacja Roguelike" />
-        <ScheduleItem
-          hour="14:45 - 15:15"
-          event="Prezentacja przedstawiciela koła naukowego GEST"
-        />
-        <ScheduleItem
-          hour="15:20 - 16:05"
-          event="Prezentacja przedstawiciela firmy Sklep E-lektronika"
-        />
-        <ScheduleItem hour="16:05 - 16:30" event="Zakończenie konferencji" />
       </div>
     </section>
   )
