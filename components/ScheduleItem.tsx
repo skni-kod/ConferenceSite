005a8eb3a46128ae53698e75cebc8cb88e2def90
Maksymilian Dendura <maksd0410@gmail.com>
2022-02-23 23:16:54 +0100

    Added organiser prop for ScheduleItem

--- components/ScheduleItem.tsx
+++ components/ScheduleItem.tsx
@@ -2,13 +2,21 @@ export type ScheduleItemProps = {
   children?: never
   event: string
   hour: string
+  organiser?: string
 }
 
-const ScheduleItem = ({ event, hour }: ScheduleItemProps) => {
+const ScheduleItem = ({ event, hour, organiser }: ScheduleItemProps) => {
   return (
-    <div className="my-3 flex justify-between rounded-xl bg-blue-50 odd:bg-blue-100  py-2 px-4 text-sm gap-4 max-w-5xl mx-auto sm:text-lg md:my-4 lg:px-12 lg:py-3 lg:text-2xl">
+    <div className="my-3 mx-auto flex max-w-5xl justify-between gap-4  rounded-xl bg-blue-50 py-2 px-4 text-sm odd:bg-blue-100 sm:text-lg md:my-4 lg:px-12 lg:py-3 lg:text-2xl">
       <div className="whitespace-nowrap">{hour}</div>
-      <div className="text-right">{event}</div>
+      <div className="text-right">
+        <div>
+          {event}
+        </div>
+        <div className="text-gray-400 italic text-xs sm:text-sm md:text-base lg:text-xl">
+          {organiser}
+        </div>
+      </div>
     </div>
   )
 }
