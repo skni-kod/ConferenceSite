72a8359c4a6ce92ace012a322ca07adada467395
Maksymilian Dendura <maksd0410@gmail.com>
2022-03-03 16:25:10 +0100

    Added partners and changed their scaling

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -2,7 +2,10 @@ import Image from './Image'
 
 const Partners = () => {
   return (
-    <section className="p-10 text-center text-xl font-semibold uppercase md:text-2xl lg:text-5xl xl:text-6xl" id="patroni">
+    <section
+      className="p-10 text-center text-xl font-semibold uppercase md:text-2xl lg:text-5xl xl:text-6xl"
+      id="patroni"
+    >
       <h2 className="py-8 normal-case lg:py-16 lg:text-4xl">
         Partnerami wydarzenia są:
       </h2>
@@ -10,7 +13,7 @@ const Partners = () => {
       <div className="mb-12 sm:mb-24">
         <h3 className="mb-6  md:mb-10 lg:mb-12">Partner główny</h3>
         <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-20 lg:h-36 w-full lg:basis-1/4">
+          <div className="relative h-20 w-full lg:h-36 lg:basis-1/4">
             <Image
               src="/deloitte.webp"
               alt="sponsor"
@@ -18,7 +21,7 @@ const Partners = () => {
               objectFit="contain"
             />
           </div>
-          <div className="relative h-20 lg:h-36 w-full lg:basis-1/4">
+          <div className="relative h-20 w-full lg:h-36 lg:basis-1/4">
             <Image
               src="/fabrity.webp"
               alt="sponsor"
@@ -29,10 +32,10 @@ const Partners = () => {
         </div>
       </div>
 
-      <div className="mb-12 sm:mb-24">
+      {/* <div className="mb-12 sm:mb-24">
         <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-4xl xl:text-5xl">Partner złoty</h3>
         <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-16 lg:h-32 w-full lg:basis-1/4">
+          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
             <Image
               src="/logo.webp"
               alt="sponsor"
@@ -46,7 +49,7 @@ const Partners = () => {
       <div className="mb-12 sm:mb-24">
         <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-4xl xl:text-5xl">Partner srebrny</h3>
         <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-14 lg:h-28 w-full lg:basis-1/4">
+          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
             <Image
               src="/logo.webp"
               alt="sponsor"
@@ -54,7 +57,7 @@ const Partners = () => {
               objectFit="contain"
             />
           </div>
-          <div className="relative h-14 lg:h-28 w-full lg:basis-1/4">
+          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
             <Image
               src="/logo.webp"
               alt="sponsor"
@@ -62,7 +65,7 @@ const Partners = () => {
               objectFit="contain"
             />
           </div>
-          <div className="relative h-14 lg:h-28 w-full lg:basis-1/4">
+          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
             <Image
               src="/logo.webp"
               alt="sponsor"
@@ -76,9 +79,41 @@ const Partners = () => {
       <div className="mb-12 sm:mb-24">
         <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-3xl xl:text-4xl">Partner brązowy</h3>
         <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-14 lg:h-28 w-full lg:basis-1/4">
+          <div className="relative h-10 lg:h-16 w-full lg:basis-1/4">
             <Image
               src="/logo.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+      </div> */}
+
+      <div className="mb-12 sm:mb-24">
+        <h3 className="mb-6  md:mb-10 md:text-xl lg:mb-12 lg:text-3xl xl:text-4xl">
+          Partner strategiczny
+        </h3>
+        <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
+            <Image
+              src="/rzit.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
+            <Image
+              src="/saits.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
+            <Image
+              src="/knml.webp"
               alt="sponsor"
               layout="fill"
               objectFit="contain"
@@ -88,65 +123,29 @@ const Partners = () => {
       </div>
 
       <div className="mb-12 sm:mb-24">
-        <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-3xl xl:text-4xl">Partner strategiczny</h3>
+        <h3 className="mb-6  md:mb-10 md:text-xl lg:mb-12 lg:text-3xl xl:text-4xl">
+          Partner medialny
+        </h3>
         <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
             <Image
-              src="/logo.webp"
+              src="/nowiny_1.webp"
               alt="sponsor"
               layout="fill"
               objectFit="contain"
             />
           </div>
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
             <Image
-              src="/logo.webp"
+              src="/nowiny_2.webp"
               alt="sponsor"
               layout="fill"
               objectFit="contain"
             />
           </div>
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
             <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="mb-12 sm:mb-24">
-        <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-3xl xl:text-4xl">Partner medialny</h3>
-        <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div className="relative h-12 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
+              src="/tvp3.webp"
               alt="sponsor"
               layout="fill"
               objectFit="contain"
