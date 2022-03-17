f28110a2e4d4955cd1ffb7343a95b4ca5c9c5aaf
Maksymilian Dendura <maksd0410@gmail.com>
2022-03-17 19:47:04 +0100

    Updated schedule content

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -18,7 +18,7 @@ const Schedule = () => {
         />
         <ScheduleItem
           hour="11:00 - 11:45"
-          event='Prelekcja - "Jak wystartować w IT?"'
+          event='"Jak wystartować w IT?"'
           organiser="Fabrity"
         />
         <ScheduleItem
@@ -29,16 +29,16 @@ const Schedule = () => {
         <ScheduleItem hour="12:25 - 13:10" event='Azure' organiser='Deloitte' />
         <ScheduleItem
           hour="13:20 - 14:05"
-          event='Prelekcja - "A może Rust?"'
+          event='"A może Rust?"'
           organiser="Maciej Sołtys, Mid Rust Developer"
         />
-        <ScheduleItem hour="14:30 - 15:00" event="Akcja dezinformaja, czyli jak nie dać się oszukać" organiser='Łukasz Sieracki i Jakub Pobuta, SAITS'/>
+        <ScheduleItem hour="14:30 - 15:00" event='"Akcja dezinformaja, czyli jak nie dać się oszukać"' organiser='Łukasz Sieracki i Jakub Pobuta, SAITS'/>
         <ScheduleItem
           hour="15:10 - 15:55"
           event="Pisanie oprogramownia w stylu Handmade"
           organiser="Paweł Dziedzic, Simplicity Games"
         />
-        <ScheduleItem hour="16:05 - 16:35" event="Interpretowalne AI - Analiza zachowań w sieci" organiser='Koło Naukowe Machine Learning'/>
+        <ScheduleItem hour="16:05 - 16:35" event='"Interpretowalne AI - Analiza zachowań w sieci"' organiser='Koło Naukowe Machine Learning'/>
         <ScheduleItem
           hour="16:35 - 16:45"
           event="Zakończenie konferencji i rozdanie nagród uczestnikom"
