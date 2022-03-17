1ee4194e9e4fef0e7fbb1fe988534779dbc7e3c7
Maksymilian Dendura <maksd0410@gmail.com>
2022-03-17 19:46:52 +0100

    Added new partners

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -29,6 +29,28 @@ const Partners = () => {
               objectFit="contain"
             />
           </div>
+          <div className="relative h-20 w-full lg:h-36 lg:basis-1/4">
+            <Image
+              src="/primebit.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-12 sm:mb-24">
+        <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-4xl xl:text-5xl">Partner srebrny</h3>
+        <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
+          <div className="relative h-16 w-full lg:h-28 lg:basis-1/4">
+            <Image
+              src="/ideo.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
         </div>
       </div>
 
@@ -46,35 +68,7 @@ const Partners = () => {
         </div>
       </div>
 
-      <div className="mb-12 sm:mb-24">
-        <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-4xl xl:text-5xl">Partner srebrny</h3>
-        <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div className="relative h-14 lg:h-24 w-full lg:basis-1/4">
-            <Image
-              src="/logo.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-        </div>
-      </div>
+      
 
       <div className="mb-12 sm:mb-24">
         <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-3xl xl:text-4xl">Partner brązowy</h3>
@@ -146,6 +140,22 @@ const Partners = () => {
           <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
             <Image
               src="/tvp3.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
+            <Image
+              src="/pci.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
+            <Image
+              src="/centrum.webp"
               alt="sponsor"
               layout="fill"
               objectFit="contain"
