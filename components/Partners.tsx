eecf6ae7c5dea3ab388ab99d3a7e9b89514b1ea8
Maksymilian Dendura <maksd0410@gmail.com>
2022-03-17 22:17:15 +0100

    Updated partners

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -37,13 +37,21 @@ const Partners = () => {
               objectFit="contain"
             />
           </div>
+          <div className="relative h-20 w-full lg:h-36 lg:basis-1/4">
+            <Image
+              src="/pci.webp"
+              alt="sponsor"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
         </div>
       </div>
 
       <div className="mb-12 sm:mb-24">
         <h3 className="mb-6  md:mb-10 lg:mb-12 md:text-xl lg:text-4xl xl:text-5xl">Partner srebrny</h3>
         <div className="flex flex-col gap-6 px-6 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-12">
-          <div className="relative h-16 w-full lg:h-28 lg:basis-1/4">
+          <div className="relative h-14 w-full lg:h-24 lg:basis-1/4">
             <Image
               src="/ideo.webp"
               alt="sponsor"
@@ -140,14 +148,6 @@ const Partners = () => {
           <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
             <Image
               src="/tvp3.webp"
-              alt="sponsor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div className="relative h-10 w-full lg:h-16 lg:basis-1/4">
-            <Image
-              src="/pci.webp"
               alt="sponsor"
               layout="fill"
               objectFit="contain"
