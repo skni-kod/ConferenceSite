8f925757bcc6fc505ec0f21eb645885a897cf0bc
Maksymilian Dendura <maksd0410@gmail.com>
2022-04-06 09:49:10 +0200

    Transmission

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,7 +1,8 @@
 import useCountDown from 'react-countdown-hook'
 import { useEffect } from 'react'
+import { TwitchStream } from 'react-twitch-stream'
 
-const initialTime = new Date('April 6, 2022 10:00:00').getTime() - Date.now()
+const initialTime = new Date('April 5, 2022 10:00:00').getTime() - Date.now()
 const interval = 1000
 
 const Counter = () => {
@@ -18,52 +19,62 @@ const Counter = () => {
 
   return (
     <section className="px-6 py-24 text-center md:py-32 md:text-2xl xl:py-44 xl:text-4xl">
-      <h2 className="font-semibold">Do wydarzenia pozostało:</h2>
-      <div className="mt-16 flex justify-center gap-6 bg-white md:gap-12 xl:gap-16">
-        <div className="w-14 md:w-24 xl:w-48">
-          <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
-            {days < 10 ? '0' + days.toFixed(0) : days.toFixed(0)}
+      {timeLeft > 0 ? (
+        <>
+          <h2 className="font-semibold">Do wydarzenia pozostało:</h2>
+          <div className="mt-16 flex justify-center gap-6 bg-white md:gap-12 xl:gap-16">
+            <div className="w-14 md:w-24 xl:w-48">
+              <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
+                {days < 10 ? '0' + days.toFixed(0) : days.toFixed(0)}
+              </div>
+              <div className="mt-2 font-semibold xl:mt-6">Dni</div>
+            </div>
+            <div className="w-14 md:w-24 xl:w-48">
+              <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
+                {hours < 10 ? '0' + hours.toFixed(0) : hours.toFixed(0)}
+              </div>
+              <div className="mt-2 font-semibold xl:mt-6">Godzin</div>
+            </div>
+            <div className="w-14 md:w-24 xl:w-48">
+              <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
+                {minutes < 10 ? '0' + minutes.toFixed(0) : minutes.toFixed(0)}
+              </div>
+              <div className="mt-2 font-semibold xl:mt-6">Minut</div>
+            </div>
+            <div className="w-14 md:w-24 xl:w-48">
+              <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
+                {seconds < 10 ? '0' + seconds.toFixed(0) : seconds.toFixed(0)}
+              </div>
+              <div className="mt-2 font-semibold xl:mt-6">Sekund</div>
+            </div>
           </div>
-          <div className="mt-2 font-semibold xl:mt-6">Dni</div>
-        </div>
-        <div className="w-14 md:w-24 xl:w-48">
-          <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
-            {hours < 10 ? '0' + hours.toFixed(0) : hours.toFixed(0)}
+          <div className="mt-8 md:mt-12 lg:mt-16">
+            Więcej na temat wydarzenia możesz przeczytać{' '}
+            <a
+              href="https://fb.me/e/275QyT5v7"
+              className="font-semibold uppercase text-blue-800 hover:text-blue-400"
+            >
+              tutaj
+            </a>
           </div>
-          <div className="mt-2 font-semibold xl:mt-6">Godzin</div>
-        </div>
-        <div className="w-14 md:w-24 xl:w-48">
-          <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
-            {minutes < 10 ? '0' + minutes.toFixed(0) : minutes.toFixed(0)}
+          <div className="mt-8 md:mt-12 lg:mt-16">
+            Podczas wydarzenia obowiązuje{' '}
+            <a
+              href="./konferencja/regulamin_skni_25.pdf"
+              className="font-semibold uppercase text-blue-800 hover:text-blue-400"
+              target="_blank"
+            >
+              regulamin
+            </a>
           </div>
-          <div className="mt-2 font-semibold xl:mt-6">Minut</div>
-        </div>
-        <div className="w-14 md:w-24 xl:w-48">
-          <div className="rounded-lg bg-blue-500 py-8 text-2xl font-semibold text-white shadow-md md:py-12 md:text-4xl xl:py-28 xl:text-8xl">
-            {seconds < 10 ? '0' + seconds.toFixed(0) : seconds.toFixed(0)}
-          </div>
-          <div className="mt-2 font-semibold xl:mt-6">Sekund</div>
-        </div>
-      </div>
-      <div className="mt-8 md:mt-12 lg:mt-16">
-        Więcej na temat wydarzenia możesz przeczytać{' '}
-        <a
-          href="https://fb.me/e/275QyT5v7"
-          className="font-semibold uppercase text-blue-800 hover:text-blue-400"
-        >
-          tutaj
-        </a>
-      </div>
-      <div className="mt-8 md:mt-12 lg:mt-16">
-        Podczas wydarzenia obowiązuje{' '}
-        <a
-          href="./konferencja/regulamin_skni_25.pdf"
-          className="font-semibold uppercase text-blue-800 hover:text-blue-400"
-          target="_blank"
-        >
-          regulamin
-        </a>
-      </div>
+        </>
+      ) : (
+<iframe
+    src="https://player.twitch.tv/?sknikod&parent=https://kod.prz.edu.pl/konferencja"
+    height="100"
+    width="100">
+</iframe>
+      )}
     </section>
   )
 }
